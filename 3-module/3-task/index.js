9212ee17684f0d6df5bf5ab0cdcc7cf68276d18a
skorikov-d <skorikov.denis.o@gmail.com>
2021-08-23 16:56:28 +0300

    3-module-3-task

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,10 @@
 function camelize(str) {
-  // ваш код...
+  str = str.split("-")
+  return str.map(function(item, index) {
+    if (index != 0) {
+      return item[0].toUpperCase() + item.slice(1);
+    } else {
+      return item;
+    }
+  }).join("")
 }
